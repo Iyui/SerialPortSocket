2e0999952a0604f0c9b3c30b62b7f9630a97023a
Iyui <314754801@qq.com>
2019-04-09 15:35:32 +0800

    init

---
+++ SerialPortTCP/TCPServer.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SerialPortTCP
+{
+    partial class TCPServer
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_IP = new System.Windows.Forms.TextBox();
+            this.txt_Port = new System.Windows.Forms.TextBox();
+            this.txt_Msg = new System.Windows.Forms.TextBox();
+            this.cmb_Socket = new System.Windows.Forms.ListBox();
+            this.txt_Log = new System.Windows.Forms.TextBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_IP
+            // 
+            this.txt_IP.Location = new System.Drawing.Point(134, 63);
+            this.txt_IP.Name = "txt_IP";
+            this.txt_IP.Size = new System.Drawing.Size(100, 21);
+            this.txt_IP.TabIndex = 0;
+            // 
+            // txt_Port
+            // 
+            this.txt_Port.Location = new System.Drawing.Point(332, 63);
+            this.txt_Port.Name = "txt_Port";
+            this.txt_Port.Size = new System.Drawing.Size(100, 21);
+            this.txt_Port.TabIndex = 1;
+            // 
+            // txt_Msg
+            // 
+            this.txt_Msg.Location = new System.Drawing.Point(134, 296);
+            this.txt_Msg.Name = "txt_Msg";
+            this.txt_Msg.Size = new System.Drawing.Size(280, 21);
+            this.txt_Msg.TabIndex = 2;
+            // 
+            // cmb_Socket
+            // 
+            this.cmb_Socket.FormattingEnabled = true;
+            this.cmb_Socket.ItemHeight = 12;
+            this.cmb_Socket.Location = new System.Drawing.Point(134, 179);
+            this.cmb_Socket.Name = "cmb_Socket";
+            this.cmb_Socket.Size = new System.Drawing.Size(280, 88);
+            this.cmb_Socket.TabIndex = 3;
+            // 
+            // txt_Log
+            // 
+            this.txt_Log.Location = new System.Drawing.Point(134, 341);
+            this.txt_Log.Multiline = true;
+            this.txt_Log.Name = "txt_Log";
+            this.txt_Log.Size = new System.Drawing.Size(280, 80);
+            this.txt_Log.TabIndex = 5;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(505, 63);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 6;
+            this.btn_Start.Text = "监听";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(460, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(619, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "停止监听";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn_StopListen_Click);
+            // 
+            // FrmServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.txt_Log);
+            this.Controls.Add(this.cmb_Socket);
+            this.Controls.Add(this.txt_Msg);
+            this.Controls.Add(this.txt_Port);
+            this.Controls.Add(this.txt_IP);
+            this.Name = "FrmServer";
+            this.Text = "FrmServer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_IP;
+        private System.Windows.Forms.TextBox txt_Port;
+        private System.Windows.Forms.TextBox txt_Msg;
+        private System.Windows.Forms.ListBox cmb_Socket;
+        private System.Windows.Forms.TextBox txt_Log;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
